760260b167e9482595c3ec949685df1051f3b513
pitersizovdev <pitersizov@gmail.com>
2024-04-21 21:34:57 +0300

    0.9.1

--- src/app/(routes)/screener/page.js
+++ src/app/(routes)/screener/page.js
@@ -3,13 +3,40 @@
 import Chart from "@/components/Chart/Chart";
 import styles from "./page.module.css";
 import Screen from "@/components/Screener/Screener";
-import { AdvancedChart, CryptocurrencyMarket } from "react-tradingview-embed";
+import {
+  AdvancedChart,
+  CryptocurrencyMarket,
+  Timeline,
+} from "react-tradingview-embed";
+import Card from "@/components/Card/Card";
+import Button from "@/components/Button/Button";
 
 export default function Screener() {
   return (
     <div className={styles.screener}>
-      <AdvancedChart widgetProps={{ theme: "dark" }} />;
-      <CryptocurrencyMarket widgetProps={{ theme: "dark" }} />;
+      <div className={styles.chart}>
+        <AdvancedChart
+          widgetProps={{
+            theme: "dark",
+            allow_symbol_change: true,
+            show_popup_button: true,
+            popup_width: "1000",
+            popup_height: "650",
+          }}
+        />
+      </div>
+
+      <div className={styles.market}>
+        <div className={styles.leftPanel}>
+          <CryptocurrencyMarket
+            widgetProps={{ theme: "dark", isTransparent: false }}
+          />
+        </div>
+        <div className={styles.rightPanel}>
+          <Button className="outlined">Добваить палель</Button>
+          <div className={styles.marketNews}></div>
+        </div>
+      </div>
     </div>
   );
 }
