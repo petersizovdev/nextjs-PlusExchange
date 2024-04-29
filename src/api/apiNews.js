012993aabb106d01c94f984126d216192446c276
pitersizovdev <pitersizov@gmail.com>
2024-04-29 23:01:08 +0300

    0.10.4

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -1,30 +1,16 @@
 import axios from "axios";
 
 const BASE_URL = 'https://api.currentsapi.services/v1'
-const API_KEY = 'huKoNX3n-xB-T62xvcqLD7VxqYgs84YUqY-LeRjsZVkm6vhZ'
+const API_KEY = 'nrcbrjGOxTscn42gLrH3pi38u3CvikA5W8dxCH-UgDwztfBg'
 
-export const getNews = async ({page_number = 1, page_size = 10, category, keywords}) => {
+export const getNews = async ({page_number = 1, page_size = 10, keywords}) => {
   try {
-    const response = await axios.get(`${BASE_URL}/search`, {
+    const response = await axios.get(`${BASE_URL}/search?language=ru&keywords=Крипто`, {
       params:{
         apiKey: API_KEY,
         page_number,
         page_size,
-        category,
         keywords,
-      }
-    ,})
-    return response.data;
-  } catch (error) {
-    console.log(error)
-  }
-};
-
-export const getCategories = async () => {
-  try {
-    const response = await axios.get(`${BASE_URL}/available/categories`, {
-      params:{
-        apiKey: API_KEY,
       }
     ,})
     return response.data;
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -7,8 +7,6 @@ import { getNews } from "@/api/apiNews";
 const News = () => {
   const [news, setNews] = useState([]);
 
-  const [selectedCategory, setSelectedCategory] = useState("All");
-
   const [currentPage, setCurrentPage] = useState(1);
   const pageSize = 3;
 
@@ -17,7 +15,6 @@ const News = () => {
       const response = await getNews({
         page_number: currentPage,
         page_size: pageSize,
-        category: selectedCategory === "All" ? null : selectedCategory,
       });
       setNews(response.news);
     } catch (error) {
@@ -27,7 +24,7 @@ const News = () => {
 
   useEffect(() => {
     fetchNews(currentPage);
-  }, [currentPage, selectedCategory]);
+  }, [currentPage]);
 
   return (
     <main className={styles.main}>
@@ -43,7 +40,9 @@ const News = () => {
           </div>
 
           <div className={styles.info}>
-            <p className={styles.title}>{newsItem.title}</p>
+            <p className={styles.title}>
+              {newsItem.title.split(" ").slice(0, 10).join(" ")+(newsItem.title.split(" ").length > 10 ? "..." : "")}
+            </p>
             <p className={styles.description}>
               {newsItem.description.split(" ").slice(0, 18).join(" ") +
                 (newsItem.description.split(" ").length > 18 ? "..." : "")}
