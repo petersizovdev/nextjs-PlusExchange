90df23500a0458f52029161774c9a6e6de612aba
pitersizovdev <pitersizov@gmail.com>
2024-04-29 15:52:35 +0300

    0.10.1

--- src/app/(routes)/swap/page.js
+++ src/app/(routes)/swap/page.js
@@ -4,10 +4,7 @@ import styles from "./page.module.css";
 export default function Swap() {
   return (
     <div className={styles.terminal}>
-      <div className={styles.terminalSettings}>
-        <h5>Gas:</h5>
-        <Button className="stock">⚙️</Button>
-      </div>
+   
       <input placeholder="0.0" type="number" inputMode="numeric"></input>
       <div className={styles.terminalSwap}>
         <Button className="stock">⇅</Button>
