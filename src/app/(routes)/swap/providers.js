a79ac46c98686e334895813496625486b1290c96
pitersizovdev <pitersizov@gmail.com>
2024-05-06 22:00:24 +0300

    swap

---
+++ src/app/(routes)/swap/providers.js
@@ -0,0 +1,54 @@
+"use client";
+
+import * as React from "react";
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  getDefaultConfig,
+} from "@rainbow-me/rainbowkit";
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+
+const { wallets } = getDefaultWallets();
+
+const config = getDefaultConfig({
+  appName: "0x Token Swap dApp",
+  projectId: "a8024e8262cb4e7102941a3577b5a5c0",
+  wallets: [
+    ...wallets,
+    {
+      groupName: "Other",
+      wallets: [argentWallet, trustWallet, ledgerWallet],
+    },
+  ],
+  chains: [polygon],
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }) {
+  return (
+    <div
+    >
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider>{children}</RainbowKitProvider>{" "}
+        </QueryClientProvider>
+      </WagmiProvider>
+    </div>
+  );
+}
